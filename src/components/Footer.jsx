2e8aac51d8c2d6efe3845a257605fb6048ab14c6
さいとうこうき <kooki04@saitoukoukinoMacBook-Air.local>
2026-07-11 13:43:55 +0900

    Center area titles

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,9 @@ function Footer() {
         <p>PROVIA</p>
         <p>代表　斉藤 光輝</p>
         <p>〒524-0043 滋賀県守山市二町町280-66</p>
-        <p>© 2026 PROVIA All rights reserved.</p>
+         <p>代表電話番号　080-9127-2393</p>
+         <p>E-mail provia.26.saito@gmail.com</p>
+         <p>© 2026 PROVIA All rights reserved.</p>
       </div>
     </footer>
   );
